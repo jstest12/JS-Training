2535c2deda45a32aa97f7a66f322dbf11e276626
Andrei Zhabort <esk-seen@tut.by>
2012-05-07 16:39:44 +0300

    more clean code in index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-    sendRequest('latest.json');
+    getExRates('latest.json');
 
-    if (document.getElementsByClassName == undefined) {
+    if (document.getElementsByClassName == 'undefined') {
         document.getElementsByClassName = function (cl) {
-            var retnode = [];
-            var myclass = new RegExp('\\b' + cl + '\\b');
-            var elem = this.getElementsByTagName('*');
+            var retnode = [],
+				myclass = new RegExp('\\b' + cl + '\\b'),
+				elem = this.getElementsByTagName('*');
             for (var i = 0; i < elem.length; i++) {
                 var classes = elem[i].className;
                 if (myclass.test(classes)) {
@@ -13,12 +13,12 @@ window.onload = function () {
                 }
             }
             return retnode;
-        }
-    };
-}
+        };
+    }
+};
 
 function getXmlHttp() {
-    var xmlhttp
+    var xmlhttp;
     try {
         xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
     } catch (e) {
@@ -36,33 +36,41 @@ function getXmlHttp() {
     return xmlhttp;
 }
 
-function sendRequest(option) {
-    var req = getXmlHttp()
-    req.open('GET', 'http://openexchangerates.org/' + option, true)
+function getExRates(option) {
+    var req = getXmlHttp();
+    req.open('GET', 'http://openexchangerates.org/' + option, true);
     req.onreadystatechange = function () {
-            if (req.readyState == 4) {
-                if (req.status == 200) {
-                    jsonResp(req.responseText);
-                }
+		if (req.readyState == 4) {
+            if (req.status == 200) {
+                getJsonResp(req.responseText);
             }
         }
-    req.send(null)
+    };
+    req.send(null);
 }
 
-function jsonResp(json) {
-    var jsonP = eval('(' + json + ')');
-    var myDate = new Date(jsonP.timestamp * 1000);
-    var dateStr = myDate.toLocaleDateString() + ' ' + myDate.toLocaleTimeString();
-    for (f in jsonP.rates) {
-        //		addItem3(f, jsonP.rates[f].toFixed(3));
-        if (f == 'USD') addItem3(f, jsonP.rates[f].toFixed(3));
-        if (f == 'BYR') addItem3(f, jsonP.rates[f].toFixed(3));
-        if (f == 'EUR') addItem3(f, jsonP.rates[f].toFixed(3));
-        if (f == 'RUB') addItem3(f, jsonP.rates[f].toFixed(3));
+function getJsonResp(json) {
+    var jsonP = eval('(' + json + ')'),
+		myDate = new Date(jsonP.timestamp * 1000),
+		dateStr = myDate.toLocaleDateString() + ' ' + myDate.toLocaleTimeString();
+    for (var f in jsonP.rates) {
+        //		addListItem(f, jsonP.rates[f].toFixed(3));
+        if (f == 'USD') {
+			addListItem(f, jsonP.rates[f].toFixed(3));
+		}
+        if (f == 'BYR') {
+			addListItem(f, jsonP.rates[f].toFixed(3));
+		}
+        if (f == 'EUR') {
+			addListItem(f, jsonP.rates[f].toFixed(3));
+		}
+        if (f == 'RUB') {
+			addListItem(f, jsonP.rates[f].toFixed(3));
+		}
     }
 }
 
-function addItem3(text, value) {
+function addListItem(text, value) {
     var divItem = document.createElement('div');
     divItem.className = 'item';
     var divLeft = document.createElement('div');
@@ -72,8 +80,10 @@ function addItem3(text, value) {
     var divFlag = document.createElement('div');
     divFlag.className = 'flag';
     var flagIcon = text.substr(0, 2).toLowerCase();
-    if (text == 'XAF') flagIcon = 'CF';
-    divFlag.style.cssText = 'background: url(flag-icons/flags_iso/64/' + flagIcon + '.png) no-repeat scroll 50% 50%;'
+    if (text == 'XAF') {
+		flagIcon = 'CF';
+    }
+	divFlag.style.cssText = 'background: url(flag-icons/flags_iso/64/' + flagIcon + '.png) no-repeat scroll 50% 50%;';
     var divFlagName = document.createElement('div');
     divFlagName.className = 'flag-name';
     var divFlagnameText = document.createElement('span');
@@ -105,7 +115,7 @@ function addItem3(text, value) {
     divCurrValueText.value = value;
     divCurrValueText.id = text;
     divCurrValueText.rate = value;
-    divCurrValueText.onchange = myConvert;
+    divCurrValueText.onchange = updRates;
     divCurrValue.appendChild(divCurrValueText);
     divCenterTop.appendChild(divCurrValue);
 
@@ -121,15 +131,17 @@ function addItem3(text, value) {
     container.appendChild(divItem);
 }
 
-function myConvert() {
+function updRates() {
     var curr = document.getElementsByClassName('currValueText');
-    for (f in curr) {
-        if (curr[f].id == this.id) continue;
-        curr[f].value = convert(this.rate, curr[f].rate, this.value);
+    for (var f in curr) {
+        if (curr[f].id == this.id) {
+			continue;
+		}
+        curr[f].value = convertValue(this.rate, curr[f].rate, this.value);
     }
 }
 
-function convert(from, to, startValue) {
+function convertValue(from, to, startValue) {
     var res = (to * (1 / from) * (startValue || 1)).toFixed(3);
     return res;
 }
