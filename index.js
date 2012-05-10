9fe31bb36d6ebc3018032ced7792172459f7d3ef
Andrei Zhabort <esk-seen@tut.by>
2012-05-10 13:28:08 +0300

    added constructor

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 window.onload = function () {
-	EXRATES.converter.getExRates('latest.json');
+	//EXRATES.converter.getExRates('latest.json');
+	var a = new RatesWidget('first','container');
+	a.getExRates();
+	
+	var b = new RatesWidget('second','second');
+	b.getExRates();
+	
+	var c = new RatesWidget('third','third');
+	c.getExRates();
 };
